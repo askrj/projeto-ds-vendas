06510a1d12b2e0448b37c228a535bd5a71bd4c26
Alex Santos <ask.rj10@gmail.com>
2021-05-14 17:20:47 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTeble from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo</h1>
-    </div>
+    <>
+      <div className="container">
+        <NavBar></NavBar>
+        <h1 className="text-primary">Olá Mundo</h1>
+        <DataTeble/>
+        <Footer/>
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/alexsander-santos-6812554a/" target="_blank" rel="noreferrer">Alexsander Santos</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
